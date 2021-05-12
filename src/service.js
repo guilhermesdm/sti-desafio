8ca3eb3e8980480c2f563602f8d5244a5500548e
Guilherme <guilhermesanm2003@gmail.com>
2021-05-12 19:49:07 -0300

    https

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: `http://api.openweathermap.org/data/2.5/forecast?appid=b9489da3fa908c407e9905baa3f8cfa7&units=metric&lang=pt_br`
+    baseURL: `https://api.openweathermap.org/data/2.5/forecast?appid=b9489da3fa908c407e9905baa3f8cfa7&units=metric&lang=pt_br`
 })
 
 export const fetchCidade = async (cidade) => {
